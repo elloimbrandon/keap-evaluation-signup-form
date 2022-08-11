97ddf4bc07eb796415232464004de73cc9770eba
Brandon Feltz <elloimbrandon@gmail.com>
2022-08-11 09:42:33 -0700

    react app initial setup/clean up. Intalled packages needed. axios, tailwind

---
+++ keap-signup-frontend/src/App.js
@@ -0,0 +1,20 @@
+// ** Packages **
+// (Axios) npm install axios
+// (Tailwind Css) npm install -D tailwindcss; npx tailwindcss init
+
+import "./App.css";
+// import axios from "axios"
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-Green">Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
+
+
+// notes
+// * (Tailwind color text) - text-Green
---
+++ keap-signup-frontend/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "Green": "#36A635",
+        "Blue": "#006CEB",
+        "Red": "#E02500",
+        "Black": "#0F0F0F",
+        "Grey": "#666666",
+      },
+    },
+  },
+  plugins: [],
+};
